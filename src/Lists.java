975d6b18da8fbc97cf89c894ffd123b3d6b89eab
lsplg <lvovaludmil@gmail.com>
2021-07-06 22:45:08 +0300

    add Lists

---
+++ src/Lists.java
@@ -0,0 +1,25 @@
+public class Lists {
+
+    public static long calculateSum(ArrayList list) {
+        long sum = 0;
+        for (int i = 0; i < list.size(); i++) sum += list.get(i);
+        return sum;
+    }
+
+    public static void sort(ArrayList list) {
+        for (int i = 0; i < list.size(); i++) {
+            int maxValue = list.get(0);
+            int indexOfMaxValue = 0;
+            for (int j = 1; j < list.size() - i; j++) {
+                if (maxValue < list.get(j)) {
+                    maxValue = list.get(j);
+                    indexOfMaxValue = j;
+                }
+            }
+            int tmp = maxValue;
+            list.set(indexOfMaxValue, list.get(list.size() - i - 1));
+            list.set(list.size() - i - 1, tmp);
+        }
+    }
+}
+
