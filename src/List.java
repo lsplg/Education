0397ae13f23256e2de96a76f77e5ce7d9013a7c7
lsplg <lvovaludmil@gmail.com>
2021-07-26 15:52:23 +0300

    create interface

---
+++ src/List.java
@@ -0,0 +1,10 @@
+public interface List {
+
+    int get(int index);
+    void add(int value);
+    void set(int index, int value);
+    int size();
+    void remove(int index);
+    boolean isEmpty();
+
+}
